3f636d1a3022947547fc2f1e9d118051874cb894
BelenCatalan <labdelante@gmail.com>
2020-03-22 20:57:44 +0100

    fix code work ok

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 'use strict';
 
 const btn = document.querySelector('.js-updateNumber');
-console.log(btn);
 const getNumberuser = document.querySelector('.js-getNumber');
-const givePistaUser = document.querySelector('.js-pistaUser');
+const giveClueUser = document.querySelector('.js-pistaUser');
 const giveCont = document.querySelector('.js-contTimes');
-
-//Primero generar un número aleatorio
 
 function getRandomNumber() {
   return Math.ceil(Math.random() * 100);
@@ -15,38 +12,34 @@ const randomNumber = getRandomNumber();
 
 console.log(randomNumber);
 
-//Usuario introduce un número, comprobamos que esté entre 0 y 100, comprobamos que sea igual o no a nuestro número, y devolvemos dos líneas
-function checkNumber() {
-  const number = getNumberuser.value;
-  // const randomNumber = getRandomNumber();
-  console.log(number);
-  console.log(randomNumber);
+let tries = 0;
+function numItems(ev) {
+  // ev.preventDefault();
+  tries = tries + 1;
+  giveCont.innerHTML = 'Número de intentos: ' + tries;
+}
 
+function checkNumber(ev) {
+  let number = getNumberuser.value;
+  number = parseInt(number);
+  ifStructure(number);
+}
+
+function ifStructure(number) {
   if (number < 0 || number > 100) {
-    givePistaUser.innerHTML = 'El número debe estar entre 1 y 100';
+    giveClueUser.innerHTML = 'El número debe estar entre 1 y 100';
   } else if (number === randomNumber) {
-    givePistaUser.innerHTML = 'Has ganado campeona!!!';
+    giveClueUser.innerHTML = 'Has ganado campeona!!!';
   } else if (number < randomNumber) {
-    givePistaUser.innerHTML = 'Demasiado bajo';
-  } else if (number > randomNumber) {
-    givePistaUser.innerHTML = 'Demasiado alto';
+    giveClueUser.innerHTML = 'Demasiado bajo';
   } else {
-    //do nothing
+    giveClueUser.innerHTML = 'Demasiado alto';
   }
 }
-
-// devolver el número de pruebas
-// function numItems{
-// let tries = btn.currentTarget //numero de veces que pinchemos el botton
-// tries = tries + 1;
-// giveCont.innerHTML= ('Número de intentos: ' tries)
-
-// }
-
-//llamamos a las funciones, porque todas funcionan con el boton
-function showall() {
+function showAll(ev) {
+  ev.preventDefault();
   checkNumber();
-  //numItems();
+  numItems();
 }
 
-btn.addEventListener('click', checkNumber);
+btn.addEventListener('click', showAll);
